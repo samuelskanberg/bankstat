6ea6113173143d82cc802dc415551d130170fd88
Samuel Skanberg <samuel.skanberg@gmail.com>
2015-01-26 09:10:01 +0100

    Fix indenting and removed some comments for bankstat.js

--- bankstat.js
+++ bankstat.js
@@ -1,25 +1,25 @@
 (function($) {
-	$(document).ready(function(){
-		$('#applyscriptbutton').click(function() {
-			var scriptText = $('#scripttextfield').val();
+    $(document).ready(function(){
+        $('#applyscriptbutton').click(function() {
+            var scriptText = $('#scripttextfield').val();
 
-			// For each checkbox, see if the label contains the text in the textbox
-			$("#selected_nids input[type='checkbox']").each(function() {
-				var labelText = $("label[for='"+$(this).attr("id")+"']").text();
-				if (labelText.indexOf(scriptText) >= 0) {
-					$(this).attr('checked', 'checked')
-				}
-			});
-			return false;
-		});
+            // For each checkbox, see if the label contains the text in the textbox
+            $("#selected_nids input[type='checkbox']").each(function() {
+                var labelText = $("label[for='"+$(this).attr("id")+"']").text();
+                if (labelText.indexOf(scriptText) >= 0) {
+                        $(this).attr('checked', 'checked')
+                }
+            });
+            return false;
+        });
 
-		$('#expense_tags').change(function() {
-			console.log('tick. val: '+$(this).val());
-			if ($(this).val() === "0") {
-				$('.form-item-new-tag').show();
-			} else {
-				$('.form-item-new-tag').hide();
-			}
-		});
-	});
+        $('#expense_tags').change(function() {
+            console.log('tick. val: '+$(this).val());
+            if ($(this).val() === "0") {
+                $('.form-item-new-tag').show();
+            } else {
+                $('.form-item-new-tag').hide();
+            }
+        });
+    });
 })(jQuery);
